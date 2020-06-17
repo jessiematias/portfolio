0e5fad7d279ac0ab2bb49bef282a2dc80dbb9196
jibaro <jessiegmatias@gmail.com>
2020-06-17 13:26:30 -0400

    added project

--- src/constants.js
+++ src/constants.js
@@ -1,36 +1,36 @@
 const ITEMS = [
     {
         id: '1',
-        title: "Sign-up Page",
-        description: "Sign-up page with email and password authentication. Waves animation created with CSS's @keyframe.",
+        title: "Plantco",
+        description: "E-commerce site with home page, shop page, and checkout page. Homepage contains testimonials carousel and animation scroll to bottom. Shop page allows add items to cart with hover and click. Cart icon displays popup view on desktop screens with the ability to add, substract and remove. Checkout page also allows substract, add and remove. Cart local storage is managed by Redux's persist. State is managed by Redux. Mobile responsive.",
+        technologies: "React, Redux, CSS-in-JS",
+        liveURL: "https://jessiematias.github.io/plantco-site/#/",
+        codeURL: "https://github.com/jessiematias/plantco-site/tree/master"
+    },
+    {
+        id: '2',
+        title: "Coronavirus Chart",
+        description: "Coronavirus chart created with Charts.js. Homepage chart directs user to global data and a dropdown menu allows the display of a different country's data.",
+        technologies: "React Hooks, APIs, Axios",
+        liveURL: "https://jessiematias.github.io/covid-data/",
+        codeURL: "https://github.com/jessiematias/covid-data/tree/master"
+    },
+    {
+        id: '3',
+        title: "Sign-up page",
+        description: "Sign-up page with email and password authentication through Vanilla JavaScript. Waves animation created with CSS's @keyframe",
         technologies: "SASS, JavaScript",
         liveURL: "https://jessiematias.github.io/signup-page/",
         codeURL: "https://github.com/jessiematias/signup-page"
     },
-    {
-        id: '2',
-        title: "Coronavirus map",
-        description: "Sign-up page with email and password authentication. Waves animation created with CSS's @keyframe.",
-        technologies: "SASS, JavaScript, React, Mapbox",
-        liveURL: "https://jessiematias.github.io/signup-page/",
-        codeURL: "/"
-    },
-    {
-        id: '3',
-        title: "Sign-up Page",
-        description: "Sign-up page with email and password authentication. Waves animation created with CSS's @keyframe.",
-        technologies: "SASS, JavaScript",
-        liveURL: "https://jessiematias.github.io/signup-page/",
-        codeURL: "/"
-    },
-    {
-        id: '4',
-        title: "Sign-up Page",
-        description: "Sign-up page with email and password authentication. Waves animation created with CSS's @keyframe.",
-        technologies: "SASS, JavaScript",
-        liveURL: "https://jessiematias.github.io/signup-page/",
-        codeURL: "/"
-    }
+    // {
+    //     id: '4',
+    //     title: "",
+    //     description: "",
+    //     technologies: "",
+    //     liveURL: "",
+    //     codeURL: ""
+    // }
 ]
 
 export default ITEMS
