4ee5998b0fa77dd2ee25a0e0153f9ceab2c635d8
jibaro <jessiegmatias@gmail.com>
2020-06-17 14:08:55 -0400

    Updates

---
+++ precache-manifest.b239269c16681eb3702f824085e2b132.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ea53553fde15259d1e78a71724ecae",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9f4705dd016ad2247e4a",
+    "url": "/portfolio/static/js/2.efbd705d.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/portfolio/static/js/2.efbd705d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b415b23f3b0d8c547cf2",
+    "url": "/portfolio/static/js/main.63442e3d.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "55bc2809cf19b8052a7f9f7e79780f32",
+    "url": "/portfolio/static/media/logo.55bc2809.svg"
+  },
+  {
+    "revision": "0ea489c6081aed0ff931fe39eb1e05ce",
+    "url": "/portfolio/static/media/menlo-regular-webfont.0ea489c6.svg"
+  },
+  {
+    "revision": "92e80a8beaa24b605c3e428946769385",
+    "url": "/portfolio/static/media/menlo-regular-webfont.92e80a8b.woff"
+  },
+  {
+    "revision": "995f11e3bd26d59e7d8bfa04e265b75f",
+    "url": "/portfolio/static/media/menlo-regular-webfont.995f11e3.woff2"
+  },
+  {
+    "revision": "ebfcf0b3b2d4c8ae1035f9e7978b52cd",
+    "url": "/portfolio/static/media/menlo-regular-webfont.ebfcf0b3.eot"
+  },
+  {
+    "revision": "f4343bc812f66f6a6a1201288430123c",
+    "url": "/portfolio/static/media/menlo-regular-webfont.f4343bc8.ttf"
+  }
+]);
