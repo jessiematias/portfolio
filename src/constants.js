3279eb5e732f4547e1ab8900ddb5fecd68052af3
jibaro <jessiegmatias@gmail.com>
2020-06-30 22:49:34 -0400

    added technologies

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ const ITEMS = [
         id: '1',
         title: "Plantco",
         description: "E-commerce site with home page, shop page, and checkout page. Homepage contains testimonials carousel and animation scroll to bottom. Shop page allows add items to cart with hover and click. Cart icon displays popup view on desktop screens with the ability to add, subtract and remove. Checkout page also allows subtract, add and remove. Cart's local storage is managed by Redux's persist. State is managed by Redux. Mobile responsive.",
-        technologies: "React, Redux, CSS-in-JS",
+        technologies: "React, Redux, Styled components",
         liveURL: "https://jessiematias.github.io/plantco-site/#/",
         codeURL: "https://github.com/jessiematias/plantco-site/tree/master"
     },
@@ -11,7 +11,7 @@ const ITEMS = [
         id: '2',
         title: "Hacker News clone",
         description: "Updated and modernized version of Hacker News using Hacker New's API. Navigation to newest, trending, jobs, and show stories. Each story contains a link directing to the source, username, number of comments and how long ago it was posted. Unix time code is converted to readable time. Infinite scroll with the ability to go back up with a click on the up arrow. Light and dark mode is handled with styled components' theme provider. The theme selection is saved with JavaScript's local storage. Mobile responsive.",
-        technologies: "SASS, JavaScript",
+        technologies: "React, Styled components, API, Axios",
         liveURL: "https://jessiematias.github.io/hacker-news/#/",
         codeURL: "https://github.com/jessiematias/hacker-news/tree/master"
     },
@@ -19,7 +19,7 @@ const ITEMS = [
         id: '3',
         title: "Coronavirus Chart",
         description: "Coronavirus chart created with Charts.js. Homepage chart directs user to global data and a dropdown menu allows the display of a different country's data.",
-        technologies: "React Hooks, APIs, Axios",
+        technologies: "React Hooks, API, Axios",
         liveURL: "https://jessiematias.github.io/covid-data/",
         codeURL: "https://github.com/jessiematias/covid-data/tree/master"
     },
